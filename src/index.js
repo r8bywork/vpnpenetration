e72923bfdb264864a0d3a4b894b4872f3f23d7f4
r8bywork <vanya.smolensky@gmail.com>
2022-12-27 23:12:13 +0300

    add table

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import "primereact/resources/themes/tailwind-light/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "./App.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
