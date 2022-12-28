433bc80a579e4233c560c7d82462eddb0b0a4ecd
r8bywork <vanya.smolensky@gmail.com>
2022-12-28 05:02:29 +0300

    login page auth

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -3,23 +3,43 @@ import { Button } from "primereact/button";
 import { Password } from "primereact/password";
 import { useState } from "react";
 import './Login.css'
+
 const Login = () => {
-  const [login, setLogin] = useState("")
+  const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(login)
+    console.log(username)
     console.log(password)
-    // Perform login action using the provided username and password
+    fetch('http://45.8.146.72:24678/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // username was successful, redirect to the dashboard or show a success message
+          console.log("good")
+        } else {
+          // username was unsuccessful, show an error message
+          console.log("idi naxui")
+        }
+      })
+      .catch((error) => {
+        console.log(error)
+        // There was an error connecting to the backend, show an error message
+      });
   };
   return (
     <form onSubmit={handleSubmit} className="popa">
       <label htmlFor="username">Username:</label>
       <InputText
         name="username"
-        value={login}
-        onChange={(e) => setLogin(e.target.value)}
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
 
       <label htmlFor="password">Password:</label>
